89631f863bbcf0b43775c0a1658fbf2238f231a3
LilJulian <juliadar267@gmail.com>
2024-11-18 05:35:02 -0500

    segundo avance

--- app.js
+++ app.js
@@ -115,6 +115,9 @@ else{
     console.log(julian)
 }
 
+//La funcion tipo generador nos permite
+//ingresar valores en orden y llamarlos en el 
+//orden que se escribieron
 
 {
     function* generador() {
@@ -129,5 +132,153 @@ else{
     console.log(gen.next());  
     console.log(gen.next());  
    
+}
+
+// Una arrow function o funcion flecha es la funcion 
+// que se guarda en una variable, luego se la asignan parametros
+// y luego la accion a realizar 
+
+{
+    const sumar = (a, b) => a + b;
+
+    console.log(sumar(3, 5));
+}
+
+// Un objeto en js se puede traducir como una variable 
+// que contiene varias propiedades 
+
+{
+    let estudiante = {
+        nombre : 'julian',
+        grado : 11
+    }
+    console.log(estudiante);
+}
+
+// TIPOS DE OPERADORES
+
+// Operador aritmetico
+
+operadores_arit = 5 + 5
+console.log(operadores_arit);
+
+//Operador de asignacion
+
+op_asignacion = 1
+op_asignacion += 1
+console.log(op_asignacion);
+
+// Operador de comparacion, retorna true o false
+
+console.log(3<4);
+
+// Operador logico
+
+{
+    let verdadero = true
+    let falso = false
+    console.log(a && b)  //imprime falso ya que una de las variables son falsas
+}
+
+// Operador terniario, nos permite evaluar una expresion y retornar un valor
+
+{   
+    const edad = 18;
+    const mensaje = (edad >= 18) ? "Es mayor de edad" : "Es menor de edad";
+    console.log(mensaje);
+}
+
+// Operador de concatenacion
+
+{
+    let nombre = "julian"
+    let edad = "18"
+    console.log(nombre + " tiene " + edad)
+}
+
+//Condicionales (if, else if)
+
+{
+    let nota = 1
+    if (nota > 3){ //solo si se cumplio
+        console.log("Aprobo la nota");
+    }
+    else if(nota === 3 ){ // si no se cumplio el anterior entonces el siguiente
+        console.log("Tiene que recuperar");
+    }
+    else{
+        console.log("Perdio"); //si finalmente no se cumplio la condicion
+    }
+}
+
+//CICLOS
+
+
+{
+    // El for crea una variable, luego le da una 
+    // condicion y finalmente se incrementa (en este caso)
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        
+    }
+
+    //A el while le asignamos una condicion y mientras 
+    // se cumpla se ejecutara las veces que sea 
+    // verdadero hasta que sea false
+    let cinco = 0
+    while (cinco < 5){
+        console.log("se ejecuto 5 veces")
+        cinco++;
+    }
+}
+
+// try, catch o manejo de errores son dos bloques 
+// de codigo en el que el primero intenta el codigo a ejecutar: el "try",
+// pero en el catch ejecuta un bloque por si da error
+
+{
+    try {
+        let resultado = y + 2  //no define y asi que no mostrara nada
+        console.log(resultado, "si")
+    } catch (error) {
+        console.log("Ha ocurrido un error", error.message)
+    }
+}
+
+//BREAK Y CONTINUE
+
+{
+    let numero = 0;
+    let num =0;
+
+while (numero < 5) {
+    numero++;
+    if (numero === 3) {
+        continue;  // omite el resultado del numero cuando sea exactamente igual a 3
+    }
+    console.log(numero);
+}
+console.log("")
+
+while (num < 5) {
+    num++;
+    if (num === 3) {
+        break;  // Detiene el ciclo cuando el num sea exactamente igual a 3
+    }
+    console.log(num);
+}
+
+}
+
+//Destructuracion
+
+{
+    //En esta destructuracion se le asigna a cada elemento del array 
+    //y 
+    const equipos = ["bucaramanga", "america", "cucuta"];
+    const [mejor, malo, malisimo] = equipos;
+    console.log(mejor);
+    console.log(malo);
+    console.log(malisimo);
 }
 
